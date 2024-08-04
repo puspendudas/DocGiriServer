41a3522794b5c7296e54f5b408ec70acd1fbfbe2
puspendudas <puspendudas76@gmail.com>
2024-08-04 06:21:06 +0530

    Name UPDATE

--- src/controllers/index.controller.ts
+++ src/controllers/index.controller.ts
@@ -3,7 +3,7 @@ import { NextFunction, Request, Response } from 'express';
 class IndexController {
   public index(req: Request, res: Response, next: NextFunction) {
     try {
-      res.status(200).json({'message': 'Welcome to the GocGiri!'});
+      res.status(200).json({'message': 'Welcome to the DocGiri!'});
     } catch (error) {
       next(error);
     }
